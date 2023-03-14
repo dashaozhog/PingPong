2afc83647d89c8f8d2b4972b9b157466cb7c65d7
dashaozhog <126893190+dashaozhog@users.noreply.github.com>
2023-03-14 19:49:50 +0200

    fail with ball

--- main.py
+++ main.py
@@ -3,14 +3,29 @@ from models import *
 background = transform.scale(image.load("background.png"), WIN_SETTING)
 clock = time.Clock()
 
+font.init()
+fontlabel = font.SysFont("Terminal", 32)
 
-r1 = Player(paddle_IMG, 0,0, 10, "left")
-r2 = Player(transform.flip(paddle_IMG, True, False), WIN_SETTING[0]-SPSIZE[0],0, 10, "right")
+RED = (255,0,0)
+GREEN = (0,255,0) 
+
+player1_win = fontlabel.render('Player 1 won!', True, RED)
+player2_win = fontlabel.render('Player 2 won!', True, GREEN)
+
+r1 = Player(paddle_IMG, 0,0, 15, "left")
+r2 = Player(transform.flip(paddle_IMG, True, False), WIN_SETTING[0]-SPSIZE[0],0, 15, "right")
+ball = Ball()
 game = True
+finish = False
 while game:
 	window.blit(background, (0,0))
-	r1.update_position()
-	r2.update_position()
+	if not finish:
+		r1.update_position()
+		r2.update_position()
+		ball.draw()
+		ball.move(r1,r2)
+		if ball.rect.x == 0 or ball.rect.x == WIN_SETTING[0]-ball.rect.width:
+			finish = True
 	for e in event.get():
 		if e.type == QUIT:
 			game = False
--- models.py
+++ models.py
@@ -1,36 +1,61 @@
 from pygame import *
+from random import uniform
 
 WIN_SETTING = (1800,1000)
 FPS = 60
 window = display.set_mode(WIN_SETTING)
 SPSIZE = (100, 300)
 paddle_IMG = transform.scale(image.load("paddle.png"), SPSIZE)
+vect = Vector2
 
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, player_speed, typee):
+    def __init__(self, player_image, player_x, player_y, player_speed):
         super().__init__()
         self.image = player_image
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
-        self.typee = typee
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
+    def __init__(self,  player_image, player_x, player_y, player_speed, typee):
+        super().__init__(player_image, player_x, player_y, player_speed)
+        self.typee = typee
     def update_position(self):
         keys = key.get_pressed()
         if self.typee == "left":
-            if keys[K_w]:
+            if keys[K_w] and self.rect.y>0:
                 self.rect.y -= self.speed
-            if keys[K_s]:
+            if keys[K_s] and self.rect.y < WIN_SETTING[1]-self.rect.height:
                 self.rect.y += self.speed
-        if self.typee == "right":
-            if keys[K_UP] :
+        elif self.typee == "right":
+            if keys[K_UP] and self.rect.y>0:
                 self.rect.y -= self.speed
-            if keys[K_DOWN]:
+            if keys[K_DOWN] and self.rect.y < WIN_SETTING[1]-self.rect.height:
 
                 self.rect.y += self.speed
 
         self.reset()
+class Ball(sprite.Sprite):
+    def __init__(self, color = (181, 16, 16), radius=50, speed = 20):
+        super().__init__()
+        self.color = color
+        self.radius = radius
+        self.rect = Rect(0,0,self.radius*2,self.radius*2)
+        self.rect.x = WIN_SETTING[0]/2
+        self.rect.y = WIN_SETTING[1]/2
+        self.speed = speed
+        self.direction = vect(self.speed, -self.speed/2)
+    def draw(self):
+        draw.circle(window, self.color,(self.rect.x, self.rect.y), 50)
+    def move(self, player1, player2):
+        self.rect.move_ip(self.direction)
+        if sprite.collide_rect(player1, self) or sprite.collide_rect(player2, self):
+            self.direction[0] *=-1
+            self.direction[1] *=-1
+            return True
+            return False
+
+
